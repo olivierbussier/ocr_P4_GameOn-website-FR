b7a5f8c3950a64b47aa76ea9a8222a246ec8800e
Olivier Bussier <olivier@bussier.fr>
2022-10-25 10:09:44 +0200

    Optimisation webpack

--- starterOnly/webpack.config.js
+++ starterOnly/webpack.config.js
@@ -1,89 +1,60 @@
-const Encore = require('@symfony/webpack-encore');
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-// copyplugin Webpack specific
-const { resolve: resolvePath } = require('path')
-const CopyPlugin = require('copy-webpack-plugin')
-const distPath = resolvePath(__dirname, '/build');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
 
-// Manually configure the runtime environment if not already configured yet by the "encore" command.
-// It's useful when you use tools that rely on webpack.config.js file.
-if (!Encore.isRuntimeEnvironmentConfigured()) {
-    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
-}
+const isProduction = process.env.NODE_ENV == "production";
 
-Encore
-    // directory where compiled assets will be stored
-    .setOutputPath('./build/')
-    // public path used by the web server to access the output path
-    .setPublicPath('/build')
-    // only needed for CDN's or sub-directory deploy
-    //.setManifestKeyPrefix('build/')
-    /*
-     * ENTRY CONFIG
-     *
-     * Add 1 entry for each "page" of your app
-     * (including one that's included on every page - e.g. "app")
-     *
-     * Each entry will result in one JavaScript file (e.g. guc_base.js)
-     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
-     */
-    .addEntry('app'   , './scss/style.scss')
+const stylesHandler = MiniCssExtractPlugin.loader;
 
-    // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
-    //.splitEntryChunks()
+const config = {
+  entry: {
+    app: "./js/app.js",
+    style: "./scss/style.scss",
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    clean: true,
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
 
-    // will require an extra script tag for runtime.js
-    // but, you probably want this, unless you're building a single-page app
-    .enableSingleRuntimeChunk()
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [stylesHandler, "css-loader", "postcss-loader", "sass-loader"],
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      },
 
-    /*
-     * FEATURE CONFIG
-     *
-     * Enable & configure other features below. For a full
-     * list of features, see:
-     * https://symfony.com/doc/current/frontend.html#adding-more-features
-     */
-    .cleanupOutputBeforeBuild()
-    .enableBuildNotifications()
-    .enableSourceMaps(!Encore.isProduction())
-    // enables hashed filenames (e.g. app.abc123.css)
-    //.enableVersioning(Encore.isProduction())
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+    ],
+  },
+};
 
-    .configureBabel((config) => {
-        config.plugins.push('@babel/plugin-proposal-class-properties');
-    })
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
 
-    // enables @babel/preset-env polyfills
-    .configureBabelPresetEnv((config) => {
-        config.useBuiltIns = 'usage';
-        config.corejs = 3;
-    })
-
-    // enables Sass/SCSS support
-    .enableSassLoader()
-    .enablePostCssLoader()
-
-    // enables @babel/preset-env polyfills
-    .configureBabelPresetEnv((config) => {
-        config.useBuiltIns = 'usage';
-        config.corejs = 3;
-    })
-
-// uncomment if you use TypeScript
-//.enableTypeScriptLoader()
-
-// uncomment to get integrity="..." attributes on your script & link tags
-// requires WebpackEncoreBundle 1.4 or higher
-//.enableIntegrityHashes(Encore.isProduction())
-
-// uncomment if you're having problems with a jQuery plugin
-//.autoProvidejQuery()
-
-// uncomment if you use API Platform Admin (composer require api-admin)
-  .enableReactPreset()
-
-    //.addEntry('admin', './assets/admin.js')
-
-config = Encore.getWebpackConfig()
-
-module.exports = config
+    config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
+  } else {
+    config.mode = "development";
+  }
+  return config;
+};
